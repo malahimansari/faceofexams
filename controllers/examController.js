4c0bdf1b1e50538ca45d9822c31fb8d14df5fbf9
malahimansari <malahimtech@gmail.com>
2024-03-01 00:22:16 +0500

    feat: exam id store on student role array

--- controllers/examController.js
+++ controllers/examController.js
@@ -2,6 +2,7 @@ import { validationResult } from "express-validator";
 import mongoose from "mongoose";
 import Exam from "../models/Exam.js";
 import Room from "../models/Room.js";
+import User from "../models/User.js";
 
 const createExam = async (req, res) => {
   const { room_id } = req.params;
@@ -41,9 +42,28 @@ const createExam = async (req, res) => {
       maxAttempts,
     });
 
-    
-
     await newExam.save();
+
+    const room = await Room.findById(room_id);
+
+    room.scheduledExams.push({
+      exam: newExam._id,
+      date: new Date(),
+    });
+
+    await room.save();
+
+    const studentIds = room.students.map((student) => student.user);
+
+    await User.updateMany(
+      { _id: { $in: studentIds }, "role.room_id": room_id },
+      {
+        $push: {
+          "role.$.exams": newExam._id,
+        },
+      }
+    );
+
     return res.json({ msg: "Exam created successfully", exam: newExam });
   } catch (error) {
     console.error(error.message);
